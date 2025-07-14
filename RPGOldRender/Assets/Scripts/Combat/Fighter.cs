19a7be92898b632ad54ae0aaa2b153a3c6d8a6a0
unknown <edvin.arn@hotmail.com>
2025-07-14 11:51:58 +0200

    ActionScheduler

--- RPGOldRender/Assets/Scripts/Combat/Fighter.cs
+++ RPGOldRender/Assets/Scripts/Combat/Fighter.cs
@@ -1,12 +1,43 @@
+using System;
+using RPG.Core;
+using RPG.Movement;
 using UnityEngine;
 
 namespace RPG.Combat
 {
     public class Fighter : MonoBehaviour
     {
-        public void Attack(CombatTarget target)
+        [SerializeField] private float weaponRange = 2f;
+        
+        private Transform target;
+        private void Update()
         {
-            print("I attacked you!");
+            if(target == null) return;
+            
+            if (!IsInRange())
+            {
+                GetComponent<Mover>().MoveTo(target.position);
+            }
+            else
+            {
+                GetComponent<Mover>().Stop();
+            }
+        }
+
+        private bool IsInRange()
+        {
+            return Vector3.Distance(transform.position, target.position) < weaponRange;
+        }
+
+        public void Attack(CombatTarget combatTarget)
+        {
+            GetComponent<ActionScheduler>().StartAction(this);
+            target = combatTarget.transform;
+        }
+
+        public void Cancel()
+        {
+            target = null;
         }
     }
 }
--- RPGOldRender/Assets/Scripts/Movement/Mover.cs
+++ RPGOldRender/Assets/Scripts/Movement/Mover.cs
@@ -1,23 +1,49 @@
+using System;
 using UnityEngine;
 using UnityEngine.AI;
+using RPG.Combat;
+using RPG.Core;
 
 namespace RPG.Movement
 {
     public class Mover : MonoBehaviour
     {
+        [SerializeField] private Transform target;
+
+        private NavMeshAgent navMeshAgent;
+
+        private void Start()
+        {
+            navMeshAgent = GetComponent<NavMeshAgent>();
+        }
+
         void Update()
         {
             UpdateAnimator();
         }
+
+        public void StartMoveAction(Vector3 destination)
+        {
+            GetComponent<ActionScheduler>().StartAction(this);
+            GetComponent<Fighter>().Cancel();
+            MoveTo(destination);
+        }
     
         public void MoveTo(Vector3 destination)
         {
-            GetComponent<NavMeshAgent>().destination = destination;
+            navMeshAgent.destination = destination;
+            navMeshAgent.isStopped = false;
+        }
+
+        public void Stop()
+        {
+            navMeshAgent.isStopped = true;
+            
         }
 
         private void UpdateAnimator()
         {
-            Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+            Vector3 velocity = navMeshAgent.velocity;
             Vector3 localVelocity = transform.InverseTransformDirection(velocity);
             float speed = localVelocity.z;
             GetComponent<Animator>().SetFloat("forwardSpeed", speed);
