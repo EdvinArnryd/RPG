33b7205c56c36e3e8a98c6c2b184f7b4becd9d87
Edvin <edvin.arn@hotmail.com>
2025-08-13 17:26:39 +0200

    Disable and enable controller during cutscene

--- RPGOldRender/Assets/Scripts/Cinematics/CinematicsControlRemover.cs
+++ RPGOldRender/Assets/Scripts/Cinematics/CinematicsControlRemover.cs
@@ -1,25 +1,30 @@
 using System;
+using RPG.Control;
 using UnityEngine;
 using UnityEngine.Playables;
+using RPG.Core;
 
 namespace RPG.Cinematics
 {
     public class CinematicsControlRemover : MonoBehaviour
     {
+        private GameObject player;
         private void Start()
         {
+            player = GameObject.FindWithTag("Player");
             GetComponent<PlayableDirector>().played += DisableControl;
             GetComponent<PlayableDirector>().stopped += EnableControl;
         }
 
         void DisableControl(PlayableDirector pd)
         {
-            print("DisableControl");
+            player.GetComponent<ActionScheduler>().CancelCurrentAction();
+            player.GetComponent<PlayerController>().enabled = false;
         }
 
         void EnableControl(PlayableDirector pd)
         {
-            print("EnableControl");
+            player.GetComponent<PlayerController>().enabled = true;
         }
     }
 
