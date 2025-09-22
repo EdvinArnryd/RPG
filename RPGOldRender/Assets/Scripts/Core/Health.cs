6b76d6bef173c6983261275d91459077ec3ded0b
Edvin <edvin.arn@hotmail.com>
2025-09-22 16:42:34 +0200

    Saving health Component

--- RPGOldRender/Assets/Scripts/Core/Health.cs
+++ RPGOldRender/Assets/Scripts/Core/Health.cs
@@ -1,17 +1,19 @@
 using UnityEngine;
+using RPG.Saving;
 
 namespace RPG.Core
 {
-    public class Health : MonoBehaviour
+    public class Health : MonoBehaviour, ISaveable
     {
-        [SerializeField]private float healthPoints = 100f;
-        
+        [SerializeField] private float healthPoints = 100f;
+
         private bool isDead = false;
 
         public bool IsDead()
         {
             return isDead;
         }
+
 
         public void TakeDamage(float damage)
         {
@@ -30,5 +32,22 @@ namespace RPG.Core
             isDead = true;
             GetComponent<ActionScheduler>().CancelCurrentAction();
         }
-    }
+
+
+        public object CaptureState()
+        {
+            return healthPoints;
+        }
+
+
+        public void RestoreState(object state)
+        {
+            healthPoints = (float)state;
+            
+            if (healthPoints == 0)
+            {
+                Die();
+            }
+        }
+    } 
 }
