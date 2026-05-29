ce75673ffbfa45997aa89c5f06275eb273fcfe56
EdvinArnryd <edvin.arn@hotmail.com>
2026-05-29 14:28:23 +0200

    Water navmesh

--- RPGOldRender/Assets/Scripts/Control/PlayerController.cs
+++ RPGOldRender/Assets/Scripts/Control/PlayerController.cs
@@ -5,6 +5,7 @@ using RPG.Combat;
 using RPG.Attributes;
 using System.Data;
 using UnityEngine.EventSystems;
+using UnityEngine.AI;
 
 namespace RPG.Control
 {
@@ -21,6 +22,7 @@ namespace RPG.Control
         }
 
         [SerializeField] CursorMapping[] cursorMappings = null;
+        [SerializeField] float maxNavMeshProjectionDistance = 1f;
 
         private void Awake()
         {
@@ -88,19 +90,36 @@ namespace RPG.Control
 
         private bool InteractWithMovement()
         {
-            RaycastHit hit;
-            bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
+            Vector3 target;
+            bool hasHit = RaycastNavMesh(out target);
             if (hasHit)
             {
                 if (Input.GetMouseButton(0))
                 {
-                    GetComponent<Mover>().StartMoveAction(hit.point, 1f);
+                    GetComponent<Mover>().StartMoveAction(target, 1f);
                 }
                 SetCursor(CursorType.Movement);
                 return true;
             }
 
             return false;
+        }
+
+        private bool RaycastNavMesh(out Vector3 target)
+        {
+            target = new Vector3();
+
+            RaycastHit hit;
+            bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
+            if(!hasHit) return false;
+
+            NavMeshHit navMeshHit;
+            bool hasCastToNavMesh = NavMesh.SamplePosition(
+                hit.point, out navMeshHit, maxNavMeshProjectionDistance, NavMesh.AllAreas);
+            if(!hasCastToNavMesh) return false;
+            
+            target = navMeshHit.position;
+            return true;
         }
         
         private void SetCursor(CursorType type)
